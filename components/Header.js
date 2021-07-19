72e286c723b597b715d904f86158c117549f70fd
aoponcedeleon <aoponcedeleon@up.edu.ph>
2021-07-19 23:26:25 +0800

    Added general layout of sections under header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Icon from '@material-tailwind/react/Icon';
 
 function Header() {
     return (
-        <div className='sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white'>
+        <header className='sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white'>
             <Button
                 color="gray"
                 buttonType="outline"
@@ -18,11 +18,23 @@ function Header() {
             <Icon name="description" size="5xl" color="blue"/>
             <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
 
-            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg">
+            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md transition ease-in-out duration-300">
                 <Icon name="search" size="3xl" color="gray" />
                 <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none"/>
             </div>
-        </div>
+
+            <Button
+                color="gray"
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className="ml-5 md:ml-20 h-20 w-20 border-0"
+            >
+                <Icon name="apps" size="3xl" color="gray" />
+            </Button>
+            <img loading="lazy"className="cursor-pointer h-12 w-12 rounded-full ml-2" src='https://yt3.ggpht.com/yti/APfAmoFaA7Z86aJWu6J5zqIDNSDAS7D6-jpjd8DzD-L2fw=s88-c-k-c0x00ffffff-no-rj-mo' alt="" />
+        </header>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Button from '@material-tailwind/react/Button'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Icon from '@material-tailwind/react/Icon';
+import Image from '@material-tailwind/react/Image';
 
 export default function Home() {
   return (
@@ -10,6 +13,42 @@ export default function Home() {
       </Head>
 
       <Header />
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a New Document</h2>
+
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 transition ease-in-out duration-300">
+              <Image src='https:links.papareact.com/pju' layout="fill" />
+            </div>
+            
+
+          <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">My Documents</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
 
     </div>
   )
