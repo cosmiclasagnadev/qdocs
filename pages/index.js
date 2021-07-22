3a4ab68ae3328e49274f759694cab39fe72cdce2
aoponcedeleon <aoponcedeleon@up.edu.ph>
2021-07-22 12:09:15 +0800

    Added DocumentRow component and Creating Doc Functionality

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,69 @@ import Icon from '@material-tailwind/react/Icon';
 import Image from '@material-tailwind/react/Image';
 import { getSession, useSession } from "next-auth/client";
 import Login from '../components/Login';
+import Modal from '@material-tailwind/react/Modal'
+import ModalBody from '@material-tailwind/react/ModalBody'
+import ModalFooter from '@material-tailwind/react/ModalFooter'
+import { useState } from 'react';
+import { db } from '../firebase';
+import firebase from 'firebase';
+import { useCollectionOnce, } from 'react-firebase-hooks/firestore';
+import DocumentRow from '../components/DocumentRow';
+
 
 export default function Home() {
   const [session] = useSession();
-
   if (!session) return <Login />
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
+  const [snapshot] = useCollectionOnce(db.collection('userDocs').doc(session.user.email).collection('docs').orderBy('timestamp', 'desc'));
+
+
+  const createDocument = () => {
+    if (!input) return;
+
+    db.collection('userDocs').doc(session.user.email).collection('docs').add({
+      fileName: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput('');
+    setShowModal(false);
+  };
+
+  const modal = (
+    <Modal
+    size="sm"
+    active={showModal}
+    toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="outline-none w-full"
+        placeholder="Enter name of document..."
+        oneKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Cancel
+        </Button>
+        <Button
+          color="blue"
+          onClick={createDocument}
+          ripple="light"
+        >
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
 
   return (
     <div>
@@ -19,6 +77,8 @@ export default function Home() {
       </Head>
 
       <Header />
+      {modal}
+
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
@@ -36,7 +96,7 @@ export default function Home() {
           </div>
 
           <div>
-            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 transition ease-in-out duration-300">
+            <div onClick={() => setShowModal(true)} className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 transition ease-in-out duration-300">
               <Image src='https:links.papareact.com/pju' layout="fill" />
             </div>
             
@@ -53,6 +113,15 @@ export default function Home() {
             <p className="mr-12">Date Created</p>
             <Icon name="folder" size="3xl" color="gray" />
           </div>
+        
+        {snapshot?.docs.map(doc => (
+          <DocumentRow
+            key={doc.id}
+            id={doc.id}
+            fileName={doc.data().fileName}
+            date={doc.data().timestamp}
+          />
+        ))}
         </div>
       </section>
 
