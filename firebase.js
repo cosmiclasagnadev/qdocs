ccea7746dc313c2ab4b7d387a258fc0fef6109cc
aoponcedeleon <aoponcedeleon@up.edu.ph>
2022-06-15 17:51:58 +0800

    Fixed app-breaking bug by changing nextauthredirecturl in vercel. added new images and splash for qdocs. redeployed to vercel.

--- firebase.js
+++ firebase.js
@@ -1,25 +1,23 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzkzpfGQndgWtEPnnSHww2JGGbjWteUAE",
-    authDomain: "quick-docs-b6417.firebaseapp.com",
-    projectId: "quick-docs-b6417",
-    storageBucket: "quick-docs-b6417.appspot.com",
-    messagingSenderId: "280405190155",
-    appId: "1:280405190155:web:72bcb6e68454b28d1b090e"
-  };
+	apiKey: 'AIzaSyBzkzpfGQndgWtEPnnSHww2JGGbjWteUAE',
+	authDomain: 'quick-docs-b6417.firebaseapp.com',
+	projectId: 'quick-docs-b6417',
+	storageBucket: 'quick-docs-b6417.appspot.com',
+	messagingSenderId: '280405190155',
+	appId: '1:280405190155:web:72bcb6e68454b28d1b090e',
+};
 
 // const app = (firebase.app.length != 1) ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
-console.log(firebase.app.length);
+// console.log(firebase.app.length);
 
-let app = {}
+let app = {};
 
 if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig);
+	app = firebase.initializeApp(firebaseConfig);
 } else app = firebase.app();
-
-
 
 const db = app.firestore();
 
--- pages/index.js
+++ pages/index.js
@@ -1,155 +1,164 @@
-import Button from '@material-tailwind/react/Button'
-import Head from 'next/head'
-import Header from '../components/Header'
+import Button from '@material-tailwind/react/Button';
+import Head from 'next/head';
+import Header from '../components/Header';
 import Icon from '@material-tailwind/react/Icon';
 import Image from '@material-tailwind/react/Image';
-import { getSession, useSession } from "next-auth/client";
+import { getSession, useSession } from 'next-auth/client';
 import Login from '../components/Login';
-import Modal from '@material-tailwind/react/Modal'
-import ModalBody from '@material-tailwind/react/ModalBody'
-import ModalFooter from '@material-tailwind/react/ModalFooter'
+import Modal from '@material-tailwind/react/Modal';
+import ModalBody from '@material-tailwind/react/ModalBody';
+import ModalFooter from '@material-tailwind/react/ModalFooter';
 import { useState } from 'react';
 import { db } from '../firebase';
 import firebase from 'firebase';
-import { useCollectionOnce, } from 'react-firebase-hooks/firestore';
+import { useCollectionOnce } from 'react-firebase-hooks/firestore';
 import DocumentRow from '../components/DocumentRow';
-import { useRouter } from 'next/router'
-
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  const [session] = useSession();
-  if (!session) return <Login />
-  const [showModal, setShowModal] = useState(false);
-  const [input, setInput] = useState("");
-  const [snapshot] = useCollectionOnce(db.collection('userDocs').doc(session.user.email).collection('docs').orderBy('timestamp', 'desc'));
-  const router = useRouter();
+	const [session] = useSession();
+	if (!session) return <Login />;
+	const [showModal, setShowModal] = useState(false);
+	const [input, setInput] = useState('');
+	const [snapshot] = useCollectionOnce(
+		db
+			.collection('userDocs')
+			.doc(session.user.email)
+			.collection('docs')
+			.orderBy('timestamp', 'desc'),
+	);
+	const router = useRouter();
 
+	const createDocument = () => {
+		if (!input) return;
 
+		const docsAddr = db
+			.collection('userDocs')
+			.doc(session.user.email)
+			.collection('docs');
 
-  const createDocument = () => {
-    if (!input) return;
+		docsAddr.add({
+			fileName: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
 
-    const docsAddr = db.collection('userDocs').doc(session.user.email).collection('docs');
+		let createdId = docsAddr
+			.where('fileName', '==', input)
+			.get()
+			.then((query) => {
+				query.forEach((doc) => {
+					// console.log(doc.id, " => ", doc.data);
+					router.push(`/doc/${doc.id}`);
+				});
+			})
+			.catch((error) => {
+				console.log('Error getting docs: ', error);
+			});
+		// console.log(createdId);
+		setInput('');
+		setShowModal(false);
+	};
 
-    docsAddr.add({
-      fileName: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+	const modal = (
+		<Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+			<ModalBody>
+				<input
+					type="text"
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					className="outline-none w-full"
+					placeholder="Enter name of document..."
+					onKeyDown={(e) => e.key === 'Enter' && createDocument()}
+				/>
+			</ModalBody>
+			<ModalFooter>
+				<Button
+					color="purple"
+					buttonType="link"
+					onClick={(e) => setShowModal(false)}
+					ripple="dark"
+				>
+					Cancel
+				</Button>
+				<Button color="purple" onClick={createDocument} ripple="light">
+					Create
+				</Button>
+			</ModalFooter>
+		</Modal>
+	);
 
-    let createdId = docsAddr.where('fileName', '==', input).get().then((query) => {
-      query.forEach(doc => {
-        // console.log(doc.id, " => ", doc.data);
-        router.push(`/doc/${doc.id}`)
-      });
-    }).catch((error) => {
-      console.log('Error getting docs: ', error)
-    })
-    // console.log(createdId);
-    setInput('');
-    setShowModal(false);
-  };
+	return (
+		<div>
+			<Head>
+				<title>Quick Docs</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  const modal = (
-    <Modal
-    size="sm"
-    active={showModal}
-    toggler={() => setShowModal(false)}>
-      <ModalBody>
-        <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="outline-none w-full"
-        placeholder="Enter name of document..."
-        onKeyDown={(e) => e.key === "Enter" && createDocument()}
-        />
-      </ModalBody>
-      <ModalFooter>
-        <Button
-          color="blue"
-          buttonType="link"
-          onClick={(e) => setShowModal(false)}
-          ripple="dark"
-        >
-          Cancel
-        </Button>
-        <Button
-          color="blue"
-          onClick={createDocument}
-          ripple="light"
-        >
-          Create
-        </Button>
-      </ModalFooter>
-    </Modal>
-  )
+			<Header />
+			{modal}
 
-  return (
-    <div>
-      <Head>
-        <title>Quick Docs</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+			<section className="bg-[#F8F9FA] pb-10 px-10">
+				<div className="max-w-3xl mx-auto">
+					<div className="flex items-center justify-between py-6">
+						<h2 className="text-gray-700 text-lg">Start a New Document</h2>
 
-      <Header />
-      {modal}
+						<Button
+							color="gray"
+							buttonType="outline"
+							iconOnly={true}
+							ripple="dark"
+							className="border-0"
+						>
+							<Icon name="more_vert" size="3xl" />
+						</Button>
+					</div>
 
-      <section className="bg-[#F8F9FA] pb-10 px-10">
-        <div className="max-w-3xl mx-auto">
-          <div className="flex items-center justify-between py-6">
-            <h2 className="text-gray-700 text-lg">Start a New Document</h2>
+					<div>
+						<div
+							onClick={() => setShowModal(true)}
+							className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 transition ease-in-out duration-300"
+						>
+							<Image
+								src="/assets/images/docs-blank-googlecolors.png"
+								layout="fill"
+							/>
+						</div>
 
-            <Button
-              color="gray"
-              buttonType="outline"
-              iconOnly={true}
-              ripple="dark"
-              className="border-0"
-            >
-              <Icon name="more_vert" size="3xl" />
-            </Button>
-          </div>
+						<p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
+							Blank
+						</p>
+					</div>
+				</div>
+			</section>
 
-          <div>
-            <div onClick={() => setShowModal(true)} className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 transition ease-in-out duration-300">
-              <Image src='https:links.papareact.com/pju' layout="fill" />
-            </div>
-            
+			<section className="bg-white px-10 md:px-0">
+				<div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+					<div className="flex items-center justify-between pb-5">
+						<h2 className="font-medium flex-grow">My Documents</h2>
+						<p className="mr-12">Date Created</p>
+						<Icon name="folder" size="3xl" color="gray" />
+					</div>
 
-          <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
-          </div>
-        </div>
-      </section>
-
-      <section className="bg-white px-10 md:px-0">
-        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
-          <div className="flex items-center justify-between pb-5">
-            <h2 className="font-medium flex-grow">My Documents</h2>
-            <p className="mr-12">Date Created</p>
-            <Icon name="folder" size="3xl" color="gray" />
-          </div>
-        
-        {snapshot?.docs.map(doc => (
-          <DocumentRow
-            key={doc.id}
-            id={doc.id}
-            fileName={doc.data().fileName}
-            date={doc.data().timestamp}
-          />
-        ))}
-        </div>
-      </section>
-
-    </div>
-  )
+					{snapshot?.docs.map((doc) => (
+						<DocumentRow
+							key={doc.id}
+							id={doc.id}
+							fileName={doc.data().fileName}
+							date={doc.data().timestamp}
+						/>
+					))}
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export async function getSeverSideProps(context) {
-  const session = await getSession(context);
+	const session = await getSession(context);
 
-  return {
-    props: {
-      session
-    }
-  }
+	return {
+		props: {
+			session,
+		},
+	};
 }
